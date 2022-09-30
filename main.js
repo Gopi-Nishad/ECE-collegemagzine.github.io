647fd7ff70b69d5be8a6ae59dbbbd6f29570be9d
Gopi-Nishad <114708304+Gopi-Nishad@users.noreply.github.com>
2022-09-30 14:27:39 +0530

    ECE-collegemagzine
    This is my college magzine web site.

---
+++ main.js
@@ -0,0 +1,38 @@
+// change the nav bar scroll 
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+
+
+
+
+
+
+//showw----- hide ----------nav====
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const  closeBtn = document.querySelector("#close-menu-btn");
+
+
+
+menuBtn.addEventListener('click', ()  => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+   
+})
+
+
+// close btn--------
+
+const closeNav = () => {
+    menu.style.display = "none"
+    closeBtn.style.display = "none"
+    menuBtn.style.display = "inline-block"
+}
+
+closeBtn.addEventListener('click', closeNav)
+
